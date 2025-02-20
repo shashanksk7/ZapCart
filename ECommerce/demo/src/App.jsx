ba9e05159fc51ea8e30f7e142370fe63635396df
shashanksk7 <ssk.shashank.1890@gmail.com>
2025-02-20 12:40:18 +0530

    updated

--- ECommerce/demo/src/App.jsx
+++ ECommerce/demo/src/App.jsx
@@ -3,7 +3,14 @@ import "aos/dist/aos.css";
 import { useEffect, useState } from "react";
 import Banner from "./components/Banner/Banner.jsx";
 import Navbar from "./components/Navbar/Navbar.jsx";
-import Hero from "./components/Hero/Hero.jsx"
+import Hero from "./components/Hero/Hero.jsx";
+import Popup from "./components/Popup/Popup.jsx";
+import Products from "./components/Products/Products.jsx";
+import TopProducts from "./components/TopProducts/TopProducts.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import Testimonials from "./components/Testimonials/Testimonials.jsx";
+import Subscribe from "./components/Subscribe/Subscribe.jsx"
+
 
 function App() {
   const [orderPopup, setOrderPopup] = useState(false); // ✅ Fixed useState syntax
@@ -25,7 +32,13 @@ function App() {
     <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
     <Navbar handleOrderPopup={handleOrderPopup} />
     <Hero handleOrderPopup={handleOrderPopup} />
+    <Products />
+    <TopProducts handleOrderPopup={handleOrderPopup} />
     <Banner />
+    <Subscribe />
+    <Testimonials />
+    <Footer />
+    <Popup orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
       
     
       
--- ECommerce/demo/src/main.jsx
+++ ECommerce/demo/src/main.jsx
@@ -5,8 +5,8 @@ import "./index.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
